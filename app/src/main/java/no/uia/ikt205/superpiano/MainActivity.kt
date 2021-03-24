1ff11de9f4676b53875a4d1ef9c52f710ca44736
Christian Simonsen <crsdev@gmail.com>
2021-03-24 11:02:54 +0100

    Firebase stuff

--- app/src/main/java/no/uia/ikt205/superpiano/MainActivity.kt
+++ app/src/main/java/no/uia/ikt205/superpiano/MainActivity.kt
@@ -1,16 +1,57 @@
 package no.uia.ikt205.superpiano
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import no.uia.ikt205.superpiano.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG:String = "SuperPiano:MainActivity"
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth:FirebaseAuth
 
-
+    private lateinit var piano:PianoLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+        signInAnonymously()
+
+        piano = supportFragmentManager.findFragmentById(binding.piano.id) as PianoLayout
+
+        piano.onSave = {
+            this.upload(it)
+        }
+
     }
+
+    private fun upload(file:Uri){
+
+        val ref = FirebaseStorage.getInstance().reference.child("melodies/${file.lastPathSegment}")
+        var uploadTask = ref.putFile(file)
+
+        uploadTask.addOnSuccessListener {
+            Log.d(TAG,"Saved file to fb ${it.toString()}")
+        }.addOnFailureListener{
+            Log.e(TAG,"Error saving file to fb", it)
+        }
+    }
+
+    private fun signInAnonymously(){
+        auth.signInAnonymously().addOnSuccessListener {
+            Log.d(TAG,"Login success ${it.user.toString()}")
+        }.addOnFailureListener {
+            Log.e(TAG,"Login failed", it)
+        }
+    }
+
 }
--- app/src/main/java/no/uia/ikt205/superpiano/PianoLayout.kt
+++ app/src/main/java/no/uia/ikt205/superpiano/PianoLayout.kt
@@ -1,10 +1,12 @@
 package no.uia.ikt205.superpiano
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import kotlinx.android.synthetic.main.fragment_piano.view.*
 import no.uia.ikt205.superpiano.data.Note
 import no.uia.ikt205.superpiano.databinding.FragmentPianoBinding
@@ -13,6 +15,8 @@ import java.io.FileOutputStream
 
 
 class PianoLayout : Fragment() {
+
+    var onSave:((file:Uri) -> Unit)? = null
 
     private var _binding:FragmentPianoBinding? = null
     private val binding get() = _binding!!
@@ -57,21 +61,34 @@ class PianoLayout : Fragment() {
 
         view.saveScoreBt.setOnClickListener {
             var fileName = view.fileNameTextEdit.text.toString()
-            val path = this.activity?.getExternalFilesDir(null)
-            if (score.count() > 0 && fileName.isNotEmpty() && path != null){
-                fileName = "$fileName.musikk"
-                FileOutputStream(File(path,fileName),true).bufferedWriter().use { writer ->
-                    // bufferdWriter lever her
-                    score.forEach {
-                        writer.write("${it.toString()}\n")
-                    }
+            if (score.count() > 0 && fileName.isNotEmpty()){
+                fileName = "$fileName.music"
+                val content:String = score.map{
+                    it.toString()
+                }.reduce {
+                        acc, s -> acc + s + "\n"
                 }
+                saveFile(fileName,content)
             } else {
-                /// TODO: What to do?
+                /// TODO: no music or missing file name
             }
         }
 
         return view
+    }
+
+    private fun saveFile(fileName:String,content:String){
+        val path = this.activity?.getExternalFilesDir(null)
+        if (path != null){
+            val file = File(path,fileName)
+            FileOutputStream(file,true).bufferedWriter().use { writer ->
+                writer.write(content)
+            }
+
+            this.onSave?.invoke(file.toUri());
+        } else {
+            /// Could not get external path. Warn user?
+        }
     }
 
 }
